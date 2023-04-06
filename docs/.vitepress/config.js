cd26dde47622429f69f5dc643317067f8f008c62
“adrianmaryniewski” <adrian@maryniewski.pl>
2023-04-06 02:25:00 +0200

    base /

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/brain/',
+  base: '/',
   title: "blog.maryniewski.pl",
   description: "Adrian Maryniewski's blog",
   themeConfig: {
